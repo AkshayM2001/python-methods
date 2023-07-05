60ff66fe75acd422e145024eea55c8ba0b2e4b8d
AkshayM2001 <akshaymatale2001@gmail.com>
2023-07-05 19:13:28 +0530

    set try

---
+++ Set_Method-Akshay.py
@@ -0,0 +1,25 @@
+# From :- Akshay J. Matale.
+# Batch :- Python 5:30 PM to 6:30 PM
+# Date :- 05/07/2023
+
+
+# GLOBAL VARIABLES/ITEMS
+
+fruits = {"apple", "banana", "cherry"}
+lang = {"html", "css"}
+
+# Set of fruits
+
+# Add Method
+        # This method will add anything is not set
+
+def add_thoss_(set_name, *value):
+    try:
+        print("Old list : ", set_name)
+        print("Added to list", set_name.add(value))
+    except Exception as e:
+        print(e)
+
+add_thoss_(fruits, lang)
+
+a
