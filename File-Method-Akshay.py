5bd5fcc0849aece8aa43f8fb5d5999d6c6a3fd24
AkshayM2001 <akshaymatale2001@gmail.com>
2023-07-06 18:35:35 +0530

    new file:   File-Method-Akshay.py 	new file:   first.txt

---
+++ File-Method-Akshay.py
@@ -0,0 +1,18 @@
+# From :- Akshay J. Matale.
+# Batch :- Python 5:30 PM to 6:30 PM
+# Date :- 06/07/2023
+
+
+# GLOBAL VARIABLES/ITEMS
+
+
+
+
+# Open  Method
+
+my_files = open("first.txt", "x")
+print("File created successfully!")
+
+# x for creating a new file if it doesn't exist already and w+ is used in write
+
+my_file.write("")
