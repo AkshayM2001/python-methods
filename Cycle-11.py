72a4a91709fa221517ddfb9a82db393a7de733d4
AkshayM2001 <akshaymatale2001@gmail.com>
2023-07-12 22:19:22 +0530

    complete first phase

--- Cycle-11.py
+++ Cycle-11.py
@@ -1,23 +1,27 @@
-class quantity():
-    
+# THIS PROJECT WILL FOR MATERIAL HANDLING AND RESOURCE MANAGEMENT FOR EARLY NOTIFICATIONS OF MATERIAL SHORATGE.
+# IT WILL UPDATE BOTH SUPERVISER AND MATERIAL FEEDER TO FEED THOSE LOCATION
+
+class quantity:
     def __init__ (self, ordered_quantity, mc_no, lsq):
         self.__order = ordered_quantity 
         self.used = mc_no
         self.lsq =lsq
-        self.lsq = self.__order - self.used
-
+        # self.mmq = mmq
     def plus(self, live_stock):
         self.ls = live_stock
         self.__order += self.ls
-
-    def minus(self, live_stock):
-        self.ls = live_stock
-        self.__order += self.ls
-
     def get_stock_quantity(self):
+        print("Ordered_quantity :",  self.__order, "\nUsed_quantity :",  self.used *2, "\nStocks :", self.__order - (self.used *2))
+        # print("\nMissmatched quantity :",(self.__order - (self.used *2) - ) )
 
-        print("Ordered_quantity :",  self.__order, "\nUsed_quantity :",  self.used, "\nStocks :", self.lsq)
+        dz = self.__order - (self.used *2)
 
+        # print(dz)
+        
+        if dz < 6:
+            print("Need to order next batch")
+        else:
+            print("")
 
 
 ab = quantity(0, 0, 0)
